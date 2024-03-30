54741a4b6f90fb14fe195685c9495e2a94a8948b
pflashgary <pflashgary@gmail.com>
2024-03-30 15:41:46 +0100

    This looks more like async

--- process_async/src/main.rs
+++ process_async/src/main.rs
@@ -2,7 +2,6 @@ pub mod clustering;
 pub mod statistics;
 
 use clustering::cluster_earthquake_events;
-use common::earthquake_event::EarthquakeEvent;
 use common::fetch::run_fetch;
 use statistics::calculate_all_cluster_statistics_async;
 use std::error::Error;
@@ -12,30 +11,38 @@ use std::error::Error;
 async fn main() -> Result<(), Box<dyn Error>> {
     // Define the start date as ten years ago from today
     let end_date = chrono::Utc::now().date_naive();
-
     let start_date = end_date - chrono::Duration::days(30);
 
     // Create a date iterator to fetch data monthly
     let mut current_date = start_date.clone();
-    let mut all_earthquake_events: Vec<EarthquakeEvent> = Vec::new();
+    let mut tasks = Vec::new(); // Vector to hold asynchronous tasks
 
-    while current_date <= end_date {
+    while current_date < end_date {
         let next_2day = current_date + chrono::Duration::days(2);
         let start_time = current_date.to_string();
         let end_time = next_2day.to_string();
         let min_magnitude = 3; // Set your desired minimum magnitude here
 
+        // Clone the values to pass to the asynchronous task
+        let start_time_clone = start_time.clone();
+        let end_time_clone = end_time.clone();
+
         // Log the start and end times being fetched
-        println!("Fetching data for Start: {} End: {}", start_time, end_time);
+        println!("Fetching data for Start: {} End: {}", start_time_clone, end_time_clone);
 
-        // Fetch data asynchronously for the current month
-        let earthquake_events = run_fetch(&start_time, &end_time, min_magnitude).await?; // because of ? here, what was a &'static str didn't line up with what is the Err variant type main's return type
-
-        // Append the fetched earthquake events to the list
-        all_earthquake_events.extend(earthquake_events);
+        // Spawn asynchronous task for fetching data and push it to the vector
+        tasks.push(tokio::spawn(async move {
+            run_fetch(&start_time_clone, &end_time_clone, min_magnitude).await
+        }));
 
         // Move to the next month
         current_date = next_2day;
+    }
+
+    let mut all_earthquake_events = Vec::new();
+    for task in tasks {
+        let earthquake_events = task.await??; // Unwrap the Result twice
+        all_earthquake_events.extend(earthquake_events);
     }
 
     // Set the number of clusters for k-means clustering
