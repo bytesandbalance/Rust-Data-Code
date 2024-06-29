2879b2b606fe30d595fa46229d708547037cfd11
Nick Larsen <nick.larsen@stackable.tech>
2024-06-29 19:47:58 +0200

    feat(process_async_opentelemetry): instrument main code, and move the processing out of main

--- process_async_opentelemetry/src/main.rs
+++ process_async_opentelemetry/src/main.rs
@@ -1,35 +1,78 @@
 pub mod clustering;
 pub mod statistics;
 
+use std::error::Error;
+
+use opentelemetry::{global::shutdown_tracer_provider, KeyValue};
+use opentelemetry_sdk::{trace, Resource};
+use tracing::{instrument, level_filters::LevelFilter, Instrument, Span};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Layer, Registry};
+
 use clustering::cluster_earthquake_events;
 use common::fetch::run_fetch;
 use statistics::calculate_all_cluster_statistics_async;
-use std::error::Error;
-
-use opentelemetry::global::shutdown_tracer_provider;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::Registry;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let console_env_filter = EnvFilter::builder()
+        .with_env_var("RUST_LOG")
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
+
+    let console_logger = tracing_subscriber::fmt::layer().with_filter(console_env_filter);
+
     // Change the endpoint via OTEL_EXPORTER_OTLP_ENDPOINT. Default is http://localhost:4317/
     let otlp_exporter = opentelemetry_otlp::new_exporter().tonic();
 
-    let tracer = opentelemetry_otlp::new_pipeline()
+    let otlp_tracer = opentelemetry_otlp::new_pipeline()
         .tracing()
         .with_exporter(otlp_exporter)
+        .with_trace_config(
+            trace::config().with_resource(Resource::new(vec![KeyValue::new(
+                "service.name",
+                "earthquake_tracing_app",
+            )])),
+        )
         .install_batch(opentelemetry_sdk::runtime::Tokio)?;
 
-    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
-    let subscriber = Registry::default().with(telemetry);
+    let tracing_env_filter = EnvFilter::builder()
+        // .with_env_var("RUST_LOG")
+        .with_default_directive(LevelFilter::TRACE.into())
+        .from_env_lossy();
+
+    let telemetry = tracing_opentelemetry::layer()
+        .with_tracer(otlp_tracer)
+        .with_filter(tracing_env_filter);
+    let subscriber = Registry::default().with(telemetry).with(console_logger);
     tracing::subscriber::set_global_default(subscriber)?;
 
+    process(chrono::Utc::now(), chrono::Duration::days(5 /* * 365 */)).await?;
+
+    tracing::info!("sleeping before shutting down trace providers");
+    tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+
+    shutdown_tracer_provider();
+
+    // If the program reaches this point, everything executed successfully
+    Ok(())
+}
+
+// Manually record the fields, else we get ugly debug output
+#[instrument(skip_all, fields(end_date, back_until_seconds))]
+async fn process(
+    end_date: chrono::DateTime<chrono::Utc>,
+    back_until: chrono::Duration,
+) -> Result<(), Box<dyn Error>> {
     // Define the start date as ten years ago from today
-    let end_date = chrono::Utc::now().date_naive();
-    let start_date = end_date - chrono::Duration::days(5 * 365);
+    let end_date = end_date.date_naive();
+    let start_date = end_date - back_until;
+
+    Span::current()
+        .record("end_date", format!("{end_date}"))
+        .record("back_until_seconds", back_until.num_seconds() as i64);
 
     // Create a date iterator to fetch data monthly
-    let mut current_date = start_date.clone();
+    let mut current_date = start_date;
     let mut tasks = Vec::new(); // Vector to hold asynchronous tasks
 
     while current_date < end_date {
@@ -38,19 +81,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let end_time = next_2day.to_string();
         let min_magnitude = 3; // Set your desired minimum magnitude here
 
-        // Clone the values to pass to the asynchronous task
-        let start_time_clone = start_time.clone();
-        let end_time_clone = end_time.clone();
+        tracing::info!("Fetching data");
 
-        // Log the start and end times being fetched
-        println!(
-            "Fetching data for Start: {} End: {}",
-            start_time_clone, end_time_clone
-        );
+        // We need get the current span to instrument the async call with.
+        // Otherwise, the spawned task ends up in a separate trace tree.
+        let current_span = Span::current();
 
         // Spawn asynchronous task for fetching data and push it to the vector
         tasks.push(tokio::spawn(async move {
-            run_fetch(&start_time_clone, &end_time_clone, min_magnitude).await
+            run_fetch(&start_time, &end_time, min_magnitude)
+                .instrument(current_span)
+                .await
         }));
 
         // Move to the next month
@@ -74,11 +115,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // Print each ClusterStatistics
     for cluster_statistic in clusters_statistics {
-        println!("{}", cluster_statistic);
+        tracing::info!(?cluster_statistic, "cluster statistic");
     }
 
-    shutdown_tracer_provider();
-
-    // If the program reaches this point, everything executed successfully
     Ok(())
 }
